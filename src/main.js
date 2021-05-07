1cc09bb5c4f57e1a8043b4a9f9e8dc2dd048785b
Tatsuya Miyamae <miyamae@gmail.com>
2021-05-07 14:26:11 +0900

    商品購入ボタンを追加

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
-import Vue from 'vue'
-import Client from 'shopify-buy'
+import Vue from "vue";
+import Client from "shopify-buy";
 
 const template = `
   <div class="media" v-if="product">
@@ -20,45 +20,87 @@ const template = `
       <div class="item-body-hbr" v-html="product.descriptionHtml">
       </div>
       <div class="shopping">
-        <a class="btn btn-default" :href="product.onlineStoreUrl" target="_blank" rel="nofollow">
-          <span class="fa fa-shopping-cart"></span>
-          販売サイトへ
-        </a>
+        <form class="form-inline">
+          <template v-if="product.variants.length > 1">
+            <select class="form-control" name="variant" v-model="variantId">
+              <option v-for="variant in product.variants" :value="variant.id">
+                {{ variant.title }}
+              </option>
+            </select>
+          </template>
+          <template v-else>
+            <input type="hidden" name="variant" v-model="variantId">
+          </template>
+          <input class="form-control" style="width:70px" type="number" name="quantity" min="1" v-model="quantity">
+          <button class="btn btn-default" type="button" @click="checkout">
+            <span :class="iconCheckout"></span>
+            購入する
+          </button>
+        </form>
       </div>
     </div>
   </div>
-`
+`;
 
-const cards = document.querySelectorAll('.shopify-card')
+const cards = document.querySelectorAll(".shopify-card");
 
 for (const card of cards) {
-  card.innerHTML = ''
+  card.innerHTML = "";
   var app = new Vue({
-    data: function () {
+    data: function() {
       return {
         product: null,
-        price: null
-      }
+        price: null,
+        client: null,
+        variantId: null,
+        quantity: 1,
+        progress: false,
+      };
     },
-    mounted: function () {
-      const client = Client.buildClient({
-        domain: card.getAttribute('data-shopify-domain'),
-        storefrontAccessToken: card.getAttribute('data-shopify-accesstoken')
-      })
-      client.product.fetch(card.getAttribute('data-shopify-id')).then((product) => {
-        this.product = product
-        const prices = []
-        for (const variant of product.variants) {
-          prices.push(parseInt(variant.price))
-        }
-        this.price = `￥${Math.min(...prices).toLocaleString()}`
-        if (Array.from(new Set(prices)).length > 1) {
-          this.price += ' 〜'
-        }
-      })
+    mounted: function() {
+      this.client = Client.buildClient({
+        domain: card.getAttribute("data-shopify-domain"),
+        storefrontAccessToken: card.getAttribute("data-shopify-accesstoken"),
+      });
+      this.client.product
+        .fetch(card.getAttribute("data-shopify-id"))
+        .then((product) => {
+          this.product = product;
+          const prices = [];
+          for (const variant of product.variants) {
+            prices.push(parseInt(variant.price));
+          }
+          this.price = `￥${Math.min(...prices).toLocaleString()}`;
+          if (Array.from(new Set(prices)).length > 1) {
+            this.price += " 〜";
+          }
+          this.variantId = product.variants[0].id;
+        });
     },
-    template: template
-  })
-  app.$mount()
-  card.appendChild(app.$el)
+    computed: {
+      iconCheckout: function() {
+        return this.progress
+          ? "fa fa-spinner fa-pulse fa-fw"
+          : "fa fa-shopping-bag fa-fw";
+      },
+    },
+    methods: {
+      checkout: function() {
+        this.progress = true;
+        this.client.checkout.create().then((checkout) => {
+          this.client.checkout
+            .addLineItems(checkout.id, {
+              variantId: this.variantId,
+              quantity: parseInt(this.quantity),
+            })
+            .then((checkout) => {
+              location.href = checkout.webUrl;
+            });
+        });
+      },
+    },
+    template: template,
+  });
+  app.$mount();
+  card.appendChild(app.$el);
 }
